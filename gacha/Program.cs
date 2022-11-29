7c48b0770b697a57fbd53a15308834040b6ad612
paradox <get_ant@naver.com>
2022-11-29 20:42:11 +0900

    2

--- gacha/Program.cs
+++ gacha/Program.cs
@@ -6,38 +6,34 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace gacha
+public class Characters
 {
-    public class Characters
+    public string name;
+    public int percentage;
+    public string flavorText;
+    public int rank;
+
+    public Characters(string name, int percentage, string text, int rank)
     {
-        public string name;
-        public int percentage;
-        public string flavorText;
-        public int rank;
-
-        public Characters(string name, int percentage, string text, int rank)
-        {
-            this.name = name;
-            this.percentage = percentage;
-            this.flavorText = text;
-            this.rank = rank;
-        }
-
-
+        this.name = name;
+        this.percentage = percentage;
+        this.flavorText = text;
+        this.rank = rank;
     }
-    
-    class gacha_gacha_cute
-    {
-        Characters blueCap = new Characters("BlueCap", 1, "\"The hero appears\"", 1 );
-        Characters hiruki = new Characters("Hiruki", 9, "\"Hello world\"", 2);
-        Characters reimu = new Characters("Reimu", 30, "\"Destory All Youkais!\"", 3);
-        Characters marisa = new Characters("Marisa", 30, "\"Master Spark!!\"", 3);
-        Characters alice = new Characters("Alice", 30, "\"YouKoSo\"", 3);
+}
 
-        public void Gacha()
-        {
-            Characters[] capsules =
-                {
+class gacha_gacha_cute
+{
+    Characters blueCap = new Characters("BlueCap", 1, "\"The hero appears\"", 1);
+    Characters hiruki = new Characters("Hiruki", 9, "\"Hello world\"", 2);
+    Characters reimu = new Characters("Reimu", 30, "\"Destory All Youkais!\"", 3);
+    Characters marisa = new Characters("Marisa", 30, "\"Master Spark!!\"", 3);
+    Characters alice = new Characters("Alice", 30, "\"YouKoSo\"", 3);
+
+    public void Gacha()
+    {
+        Characters[] capsules =
+            {
                 blueCap,    //1
                 hiruki,     //9
                 reimu,      //30
@@ -45,42 +41,40 @@ namespace gacha
                 alice       //30
                 };
 
-            Random rand = new Random();
-            int cumulative = 0;
-            int gift = 0;
+        Random rand = new Random(DateTime.Now.Millisecond);
+        int cumulative = 0;
+        int gift = 0;
 
-            for (int i = 0; i < capsules.Length; i++)
+        for (int i = 0; i < capsules.Length; i++)
+        {
+            int gacha = rand.Next(0, 101);
+            cumulative += capsules[i].percentage;
+
+            if (gacha <= cumulative)
             {
-                int gacha = rand.Next(0, 101);
-                cumulative += capsules[i].percentage;
-
-                Console.WriteLine("누적 : " + cumulative);
-                Console.WriteLine("랜덤 : " + gacha);
-
-
-                if (gacha <= cumulative)
-                {
-                    gift = i;
-                    break;
-                }
+                gift = i;
+                break;
             }
-
-            Console.WriteLine(capsules[gift].name);
         }
-    }
 
-    
+        if(capsules[gift].rank == 1)
+            Console.Write(capsules[gift].flavorText + "  ★");
+
+        Console.WriteLine(capsules[gift].name);
+
+    }
+}
+namespace gacha
+{
     internal class Program
     {
         static void Main(string[] args)
         {
-            for(int i = 0; i < 100; i++)
-            {
-                gacha_gacha_cute machine = new gacha_gacha_cute();
-                machine.Gacha();
-            }
-            
-            
+            gacha_gacha_cute machine = new gacha_gacha_cute();
+            machine.Gacha();
+            machine.Gacha();
+            machine.Gacha();
+
 
         }
     }
